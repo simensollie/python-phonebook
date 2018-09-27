d55952fe73f97e980aa1be34422fa5b5da3caada
Simen Sollie <simensollie@users.noreply.github.com>
2018-09-27 18:11:06 +0200

    Add files via upload

--- phonebook.py
+++ phonebook.py
@@ -1,61 +1,71 @@
 import sys
 
-# Creating a phonebook
-phonebook = {0: {'firstname': 'Sander', 'surname': 'Sollie', 'phonenumber': '98078029', 'address': 'Agnes Mowinckels gate 6'}, 
-			1: {'firstname': 'Simen', 'surname': 'Sollie', 'phonenumber': '97616675', 'address': 'Buggelandsbakken 56'}, 
-			2: {'firstname': 'Mathilde', 'surname': 'Sollie', 'phonenumber': '98765432', 'address': 'Kornveien 4'}}
+# Creating a phone book with example entries
+phonebook = {0: {'First name': 'Sander', 'Last name': 'Sollie', 'Phone': '98078029', 'Address': 'Agnes Mowinckels gate 6'},
+             1: {'First name': 'Simen', 'Last name': 'Sollie', 'Phone': '97616675', 'Address': 'Buggelandsbakken 56'},
+             2: {'First name': 'Mathilde', 'Last name': 'Sollie', 'Phone': '98765432', 'Address': 'Kornveien 4'}}
+
+# Function for listing the records in phone book
+def list_phonebook():
+    for pid in phonebook:
+        print('\npid:', pid + 1)
+        for p_info in phonebook[pid]:
+            print(p_info + ':', phonebook[pid][p_info])
 
 # Function for adding a record to the phonebook and checking for duplicates
-def addRecord():
+def add_record():
+    print("Enter the information of the person you'd like to add")
     firstname = input('First name: ')
-    surname = input('Last name: ')
-    phonenumber = input('Phone number: ')
+    lastname = input('Last name: ')
+    phone = input('Phone: ')
     address = input('Address: ')
 
-    for pid in phonebook:
-        print(phonebook[pid].get('phonenumber'))
+    #for pid in phonebook:
+    #    print(phonebook[pid].get('Phone'))
 
     for pid in phonebook:
-        if phonenumber == phonebook[pid].get('phonenumber'):
-            print('The contact is already in the phonebook')
+        if phone in phonebook[pid]['Phone']:
+            print('The contact is already in the phone book')
+            break
+        elif phone not in phonebook[pid]['Phone']:
+            p_len = len(phonebook)
+            phonebook[p_len] = {}
+            phonebook[p_len]['First name'] = firstname
+            phonebook[p_len]['Last name'] = lastname
+            phonebook[p_len]['Phone'] = phone
+            phonebook[p_len]['Address'] = address
+            print('The contact has been added to the phone book')
+        return True
+
+
+'''
+    for pid in phonebook:
+        if phone is phonebook[pid].get('Phone'):
+            print('The contact is already in the phone book')
             break
         else:
             phonebook_length = len(phonebook)
             phonebook[phonebook_length] = {}
-            phonebook[phonebook_length]['firstname'] = firstname
-            phonebook[phonebook_length]['surname'] = surname
-            phonebook[phonebook_length]['phonenumber'] = phonenumber
-            phonebook[phonebook_length]['address'] = address
-            print('The contact has been added to the phonebook')
+            phonebook[phonebook_length]['First name'] = firstname
+            phonebook[phonebook_length]['Last name'] = lastname
+            phonebook[phonebook_length]['Phone'] = phone
+            phonebook[phonebook_length]['Address'] = address
+            print('The contact has been added to the phone book')
         return True
+'''
+# Deletes a contact from phone book based on first and last name
+def delete_record():
+    print("Enter the name of the person you'd like to remove")
+    firstname = input('First name: ')
+    lastname = input('Last name: ')
 
-def deleteRecord(name):
-    deletedList = phonebook.popitem()
-    print(phonebook)
 
-def noDuplicate(name):
-    if phonebook.get(name):
-        print('The contact is already in the phonebook')
-    else:
-        phonebook[name] = True
-        print('The contact can be added to the phonebook')
-
-menu = {}
-menu['1'] = 'Add a new record'
-menu['2'] = 'Delete a record'
-menu['3'] = 'Check if a person is in the phonebook'
-menu['4'] = 'Quit/exit'
-
-# Main function where we create menu and choose action
+# Main function where we create menu and receive input
 def mainmenu():
     while True:
-        options = list(menu.keys())
-        options.sort()
-
-        for entry in options:
-            selection = input("""
-            
-   ----MAIN MENU----
+        selection = input("""
+        
+----MAIN MENU----
 1: List phonebook
 2: Add a new record
 3: Delete a record
@@ -64,20 +74,17 @@ def mainmenu():
 
 Please enter your choice: """)
 
-            if selection == '1':
-                for pid in phonebook:
-                    print('\npid:', pid+1)
-                    for person_info in phonebook[pid]:
-                        print(person_info + ':', phonebook[pid][person_info])
-            elif selection == '2':
-                addRecord()
-            elif selection == '3':
-                deleteRecord()
-            elif selection == '4':
-                print('Checking')
-            elif selection == '5':
-                return False
-            else:
-                return True
+        if selection == '1':
+            list_phonebook()
+        elif selection == '2':
+            add_record()
+        elif selection == '3':
+            delete_record()
+        elif selection == '4':
+            print('Checking')
+        elif selection == '5':
+            return False
+        else:
+            return True
 
 mainmenu()
