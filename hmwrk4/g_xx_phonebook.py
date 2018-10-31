12f1a3c8901dc06bcfbc7d02cdacfbdc75e5d6cc
Simen Sollie <simensollie@users.noreply.github.com>
2018-10-31 22:04:02 +0100

    1,5 out of 4 methods
    addcontact - works
    search - not implemented
    delete - partly works
    listByType - not implemented

--- hmwrk4/g_xx_phonebook.py
+++ hmwrk4/g_xx_phonebook.py
@@ -1,28 +1,21 @@
-class PhoneBook(object):
-    def addContact(self):
-
-
-    def search(self):
-
-
-    def delete(self):
-
-
-    def listByType(self):
-
-
-
 class Contact(object):
     def __init__(self, name, surname, phone):
         self.name = name
         self.surname = surname
         self.phone = phone
 
+    def __str__(self):
+        s = '\nname: ' + self.name + ' ' + self.surname
+        s += '\nphone: ' + self.phone
+        return s
+
+
 class BusinessContact(Contact):
     def __init__(self, name, surname, phone, company, businessAdress):
         Contact.__init__(self, name, surname, phone)
         self.company = company
         self.businessAddress = businessAdress
+
 
 class PersonalContact(Contact):
     def __init__(self, name, surname, phone, homeAddress, relationship):
@@ -31,184 +24,96 @@ class PersonalContact(Contact):
         self.relationship = relationship
 
 
+class PhoneBook(object):
+    def __init__(self):
+        self.contacts = {}
 
-''' OLD IMPLEMENTATION
+    def __add__(self, name, surname, phone):
+        c = Contact(name, surname, phone)
+        self.contacts[name + ' ' + surname] = c
 
-""" Nested dictionary (phone book) with example entries. """
-phonebook = {0: {'First name': 'Matt', 'Last name': 'Damon', 'Phone': '95487621', 'Address': 'Celeb Street 34'},
-             1: {'First name': 'Ben', 'Last name': 'Affleck', 'Phone': '47516448', 'Address': 'Celeb Street 56'},
-             2: {'First name': 'Jimmy', 'Last name': 'Kimmel', 'Phone': '41785435', 'Address': 'Celeb Street 49'},
-             3: {'First name': 'Sarah', 'Last name': 'Silverman', 'Phone': '41785435', 'Address': 'Celeb Street 13'}}
+    def __call__(self, name):
+        return self.contacts[name]
 
+    def __str__(self):
+        s = ''
+        for p in sorted(self.contacts):
+            s += '\n' + str(self.contacts[p])
+        return s
 
-def list_phonebook(d):
-    """ List the people in your phone book. """
+    def addContact(self):
+        print("\nEnter the information of the contact you'd like to add")
+        name = input('First name: ')
+        surname = input('Last name: ')
+        phone = input('Phone: ')
+        selection = input("""
+1: Business
+2: Personal
+Other: Neither
 
-    for pid in d:
-        print('\nPID:', pid + 1)
-        for p_info in d[pid]:
-            print(p_info + ':', d[pid][p_info])
+Personal or Business? Enter: """)
 
+        if selection == '1':
+            company = input('\nCompany: ')
+            businessAddress = input('Address: ')
+            c = BusinessContact(name, surname, phone, company, businessAddress)
+        elif selection == '2':
+            homeAddress = input('\nAddress: ')
+            relationship = input('Raltionship: ')
+            c = PersonalContact(name, surname, phone, homeAddress, relationship)
+        else:
+            c = Contact(name, surname, phone)
 
-def add_record(d):
-    """ Add a new record to the phone book that avoids to add duplicates. """
+        self.contacts[name + ' ' + surname] = c
+        print("\n# Contact added")
 
-    print("\nEnter the information of the person you'd like to add")
-    firstname = input('First name: ')
-    lastname = input('Last name: ')
-    phone = input('Phone: ')
-    address = input('Address: ')
+    def delete(self):
+        print("\nEnter the name of the contact you'd like to delete")
+        name = input('First name: ')
+        surname = input('Last name: ')
 
-    name_is_equal = False
+        del self.contacts[name + ' ' + surname]
+        print("\n# Contact deleted")
 
-    for pid in d:
-        if firstname == d[pid].get('First name') and lastname == d[pid].get('Last name'):
-            name_is_equal = True
-
-    if name_is_equal is True:
-        print('\n# The contact is already in the phone book')
-    else:
-        d[len(d)] = {'First name': firstname, 'Last name': lastname, 'Phone': phone, 'Address': address}
-        print('\n# The contact has been added to the phone book')
-
-    return d
-
-
-def delete_record(d):
-    """ Delete a record from the phone book using the combination First name and Last name. """
-
-    print("\nEnter the name of the person you'd like to remove")
-    firstname = input('First name: ')
-    lastname = input('Last name: ')
-
-    for pid in d:
-        if firstname == d[pid].get('First name') and lastname == d[pid].get('Last name'):
-            del d[pid]
-            print('\n# The contact has been deleted')
-            return d
-    print('\n# The contact is not in the phone book')
-
-
-def check_record(d):
-    """ Check if a person is already in your phone book using the combination First name and Last name. """
-
-    print("\nEnter the name of the person you'd like to check")
-    firstname = input('First name: ')
-    lastname = input('Last name: ')
-
-    for pid in d:
-        if firstname == d[pid].get('First name') and lastname == d[pid].get('Last name'):
-            print('\n# The contact is already in the phone book')
-            return d
-    print('\n# The contact is not in the phone book')
+    def listByType(self):
+        s = ''
+        for p in sorted(self.contacts):
+            s += '\n' + str(self.contacts[p])
+        return s
 
 
-def change_entry_in_record(d):
-    """ Change a specific entry of a record. """
-
-    print("\nEnter the name of the person for which you'd like to change information")
-    firstname = input('First name: ')
-    lastname = input('Last name: ')
-
-    for pid in d:
-        if firstname == d[pid].get('First name') and lastname == d[pid].get('Last name'):
-            print("\n# We have located your friend. Please enter which entry you'd like to change.\n"
-                  "1: First name\n"
-                  "2: Last name\n"
-                  "3: Phone number\n"
-                  "4: Address")
-            entry = input("Enter a number between 1-4: ")
-
-            if entry == '1':
-                d[pid]['First name'] = input('Enter a new first name: ')
-                print('\n# First name has been changed')
-            elif entry == '2':
-                d[pid]['Last name'] = input('Enter a new last name: ')
-                print('\n# Last name has been changed')
-            elif entry == '3':
-                d[pid]['Phone'] = input('Enter a new phone number: ')
-                print('\n# Phone number has been changed')
-            elif entry == '4':
-                d[pid]['Adress'] = input('Enter a new address: ')
-                print('\n# Address has been changed')
-            else:
-                print('\n# 404 - Page cannot be found')
-
-            return d
-    print('# The contact is not in the phone book')
-
-
-def ordered_list_by_first_name(d):
-    """ List the people in your phone book ordered by First name. """
-
-    ordered_d = dict(sorted(d.items(), key=lambda x: x[1]['First name']))
-
-    print("\nPhone book ordered by first name: ")
-    for pid, info in ordered_d.items():
-        print('\nPerson number: ', pid)
-        for key in info:
-            print(key + ':', info[key])
-
-
-def ordered_list_by_last_name(d):
-    """ List the people in your phone book ordered by Last name. """
-
-    ordered_d = dict(sorted(d.items(), key=lambda x: x[1]['Last name']))
-
-    print("\nPhone book ordered by last name: ")
-    for pid, info in ordered_d.items():
-        print('\nPerson number: ', pid)
-        for key in info:
-            print(key + ':', info[key])
-
-
-def check_for_duplicate_phone_numbers(d):
-    """ Check if there are records having the same phone number and list them. """
-
-    print('# This function is under maintenance. Please try again later.')
-    return d
-
-
-def mainmenu():
-    """ Main function where we create menu and receive input """
+def main():
+    p = PhoneBook()
+    p.__add__('Johnny', 'Depp', '32165487')
+    p.__add__('Orlando', 'Bloom', '98765432')
+    p.__add__('Keira', 'Knightly', '12345678')
 
     while True:
         selection = input("""
-        
-----MAIN MENU----
-1: List phonebook
-2: Add a new record
-3: Delete a record
-4: Check if a person is already in your phone book
-5: Change a specific entry of a record
-6: List the people in your phone book ordered by first name
-7: List the people in your phone book ordered by last name
-8: List records with duplicate phone numbers
-9: Explode into oblivion
 
-Please enter your choice: """)
+    ----MAIN MENU----
+    1: Add contact
+    2: Search
+    3: Delete
+    4: List phone book by type
+    5: Explode into oblivion
+
+    Please enter your choice: """)
 
         if selection == '1':
-            list_phonebook(phonebook)
+            p.addContact()
         elif selection == '2':
-            add_record(phonebook)
+            print('\n# 404 - Page cannot be found')
         elif selection == '3':
-            delete_record(phonebook)
+            p.delete()
         elif selection == '4':
-            check_record(phonebook)
+            p.listByType()  # NOT WORKING
+            print(p)        # defined by method -> def __str__(self):
         elif selection == '5':
-            change_entry_in_record(phonebook)
-        elif selection == '6':
-            ordered_list_by_first_name(phonebook)
-        elif selection == '7':
-            ordered_list_by_last_name(phonebook)
-        elif selection == '8':
-            check_for_duplicate_phone_numbers(phonebook)
-        elif selection == '9':
             print('# BOOM!')
             return False
         else:
             print('\n# 404 - Page cannot be found')
 
 
-mainmenu()
+main()
