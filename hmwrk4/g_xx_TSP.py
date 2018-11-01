19c5090f0554a0a52920678c18e65079b34313a4
Simen Sollie <simensollie@users.noreply.github.com>
2018-11-01 18:34:08 +0100

    hmwrk4 prblm2
    created file and added required classes and methods

---
+++ hmwrk4/g_xx_TSP.py
@@ -0,0 +1,27 @@
+'''
+Table 1: Distances between cities. The table shows the traveling distance from the on the left
+margin to the city on the top margin. Note that the table is not symmetric.
+
+                Oslo    Bergen  Stavanger   Trondheim   Kristiansand
+Oslo            -       6.47    7.10        6.10        3.56
+Bergen          7.14    -       4.42        9.30        7.39
+Stavanger       7       4.48    -           13.37       3.14
+Trondheim       6.24    9.34    13.36       -           10.12
+Kristiansand    4.3     7.45    3.33        10.17       -
+'''
+
+
+class City(object):
+    def __init__(self):
+        return
+
+
+class Tour(object):
+    def __init__(self):
+        return
+
+    def addCity(self):
+        return
+
+    def findTour(self):
+        return
